1588219cf6a73d8774051436be720707e11f2edf
Eduardo <eduardosaes@hotmail.com>
2016-07-15 15:12:51 -0430

    agregado informacion de jefes

--- contacto.php
+++ contacto.php
@@ -1,10 +1,10 @@
 <div class="organigrama">
 	<ul>
 		<li>
-			<a href="#" onclick="mostrar('jefe');"><img src="img/contacto/jefe.jpg" alt=""><br><b>Director</b></a>	
+			<a data-toggle="modal" href="#director"><img src="img/contacto/jefe.jpg" alt=""><br><b>Director</b></a>	
 			<ul>
 				<li>
-					<a href="#" onclick="Cordinadora('coor');"><img src="img/contacto/DSCN7509.jpg" alt=""><br><b>Cordinadora general</b></a>
+					<a href="#"><img src="img/contacto/DSCN7509.jpg" alt=""><br><b>Cordinadora general</b></a>
 					<ul>
 				        <li><a href="#"><img src="img/contacto/DSCN7552.jpg" alt=""><br><b>Unidad de registro <br>y control de aporte</b></a></li>
 				        <li><a href="#"><img src="img/contacto/DSCN7564.jpg" alt=""><br><b>Unidad de <br>asesoria legal</b></a></li>
@@ -44,43 +44,20 @@
 
 <!--VENTANAS MODALES INFORMACION DE ORGANIGRAMA-->
 
-<div onclick="ocultar()" id="jefe" class="modal" style="display:none">
-  	<div class="media" id="contenido-interno">
-	  <div class="media-left media-middle">
-	    <a href="#">
-	    <img src="img/contacto/jefe.jpg" alt="" class="media-object">
-	    </a>
-	  </div>
-	  <div class="media-body">
-	    <h4 class="media-heading">Director</h4>
-	    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam fuga harum dolores quos maxime nihil consectetur officiis fugiat, reiciendis eveniet, nobis ullam quaerat vel eos quam adipisci perspiciatis nostrum fugit.
-	  </div>
-	</div>
+<div id="director" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/jefe.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Middle aligned media</h4>
+    ...
+  </div>
 </div>
-
-
-<div onclick="ocultar()" id="coor" class="modal" style="display:none">
-  	<div class="media" id="contenido-interno">
-	  <div class="media-left media-middle">
-	    <a href="#">
-	    <img src="img/contacto/jefe.jpg" alt="" class="media-object">
-	    </a>
-	  </div>
-	  <div class="media-body">
-	    <h4 class="media-heading">Director</h4>
-	    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam fuga harum dolores quos maxime nihil consectetur officiis fugiat, reiciendis eveniet, nobis ullam quaerat vel eos quam adipisci perspiciatis nostrum fugit.
-	  </div>
-	</div>
+    </div>
+  </div>
 </div>
-
-
-
-<script type="text/javascript">
-function mostrar(img) {
-document.getElementById(img).style.display = "block" ; // permite asignar display:block al elemento #modal
-}
-function ocultar() {
-document.getElementById('jefe').style.display = "none" ; // permite ocultar el contenedor al hacer clic en alguna parte de éste mediante display:none en el elemento #modal
-}
-
-</script>
