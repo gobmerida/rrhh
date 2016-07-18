2b86705eac4c06ecdfb213a919c58db555b8be84
Eduardo <eduardosaes@hotmail.com>
2016-07-18 09:51:37 -0430

    terminado modulo de contacto

--- contacto.php
+++ contacto.php
@@ -4,27 +4,27 @@
 			<a data-toggle="modal" href="#director"><img src="img/contacto/jefe.jpg" alt=""><br><b>Director</b></a>	
 			<ul>
 				<li>
-					<a href="#"><img src="img/contacto/DSCN7509.jpg" alt=""><br><b>Cordinadora general</b></a>
+					<a data-toggle="modal" href="#cordinadora"><img src="img/contacto/DSCN7509.jpg" alt=""><br><b>Coordinadora general</b></a>
 					<ul>
-				        <li><a href="#"><img src="img/contacto/DSCN7552.jpg" alt=""><br><b>Unidad de registro <br>y control de aporte</b></a></li>
-				        <li><a href="#"><img src="img/contacto/DSCN7564.jpg" alt=""><br><b>Unidad de <br>asesoria legal</b></a></li>
+				        <li><a data-toggle="modal" href="#aporte"><img src="img/contacto/DSCN7552.jpg" alt=""><br><b>Unidad de registro <br>y control de aporte</b></a></li>
+				        <li><a data-toggle="modal" href="#asesoria"><img src="img/contacto/DSCN7564.jpg" alt=""><br><b>Unidad de <br>asesoria legal</b></a></li>
 
-				        <li><a href="#"><img src="img/contacto/DSCN7526.jpg" alt=""><br><b>Unidad de <br>planificacíon y <br>presupuesto</b></a></li>
+				        <li><a data-toggle="modal" href="#presupuesto"><img src="img/contacto/DSCN7526.jpg" alt=""><br><b>Unidad de <br>planificacíon y <br>presupuesto</b></a></li>
 			    	</ul>
 				</li>	
 			</ul>
 		</li>
 	</ul><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 	<ul class="dpto">
-		<li><a href="#"><img src="img/contacto/16316852.jpg" alt=""><br><b>Dpto. <br>informatica</b></a></li>
+		<li><a data-toggle="modal" href="#informatica"><img src="img/contacto/16316852.jpg" alt=""><br><b>Dpto. <br>informatica</b></a></li>
 
-		<li><a href="#"><img src="img/contacto/DSCN7543.jpg" alt=""><br><b>Dpto. <br>Personal obrero</b></a></li>
-		<li><a href="#"><img src="img/contacto/DSCN7586.jpg" alt=""><br><b>Dpto. <br>Empleados</b></a></li>
+		<li><a data-toggle="modal" href="#obrero"><img src="img/contacto/DSCN7543.jpg" alt=""><br><b>Dpto. <br>Personal obrero</b></a></li>
+		<li><a data-toggle="modal" href="#empleado"><img src="img/contacto/DSCN7586.jpg" alt=""><br><b>Dpto. <br>Empleados</b></a></li>
 
-		<li><a href="#"><img src="img/contacto/DSCN7553.jpg" alt=""><br><b>Dpto. <br>Jubilados y pensionados</b></a></li>
-		<li><a href="#"><img src="img/contacto/DSCN7636.jpg" alt=""><br><b>Dpto. <br>Pasivos laborales</b></a></li>
+		<li><a data-toggle="modal" href="#jubilado"><img src="img/contacto/mariani.jpg" alt=""><br><b>Dpto. <br>Jubilados y pensionados</b></a></li>
+		<li><a data-toggle="modal" href="#pasivo"><img src="img/contacto/DSCN7636.jpg" alt=""><br><b>Dpto. <br>Pasivos laborales</b></a></li>
 
-		<li><a href="#"><img src="img/contacto/DSCN7517.jpg" alt=""><br><b>Dpto. <br>Administración</b></a></li>		
+		<li><a data-toggle="modal" href="#admin"><img src="img/contacto/DSCN7517.jpg" alt=""><br><b>Dpto. <br>Administración</b></a></li>		
 	</ul>
 </div>
 <marquee class="hidden-xs marq" direction="left" onmouseout="this.start();" onmouseover="this.stop();" scrollamount="4">
@@ -47,17 +47,237 @@
 <div id="director" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
+      	<div class="media">
+	  		<div class="media-left media-middle">
+		    	<a href="#"><img src="img/contacto/jefe.jpg" alt="" class="media-object"> </a>
+		 	 	</div>
+		  		<div class="media-body">
+		   	 		<h4 class="media-heading">Middle aligned media</h4>
+					Director del departamento de recursos humanos
+		  		</div>
+			</div>
+    	</div>
+  	</div>
+</div>
+
+<!--#################CORDINADORA####################-->
+
+<div id="cordinadora" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
       <div class="media">
   <div class="media-left media-middle">
     <a href="#">
-      <img src="img/contacto/jefe.jpg" alt="" class="media-object">
+      <img src="img/contacto/DSCN7509.jpg" alt="" class="media-object">
     </a>
   </div>
   <div class="media-body">
-    <h4 class="media-heading">Middle aligned media</h4>
-    ...
+    <h4 class="media-heading">Fereda Sanchez Dayari V.</h4>
+    Coordinadora general
   </div>
 </div>
     </div>
   </div>
 </div>
+
+<!--#####################################-->
+
+<!--################APORTE#####################-->
+
+<div id="aporte" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7552.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Rivas de G. Betty del C.</h4>
+    Jefa de la unidad de registro de aportes
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################asesoria#####################-->
+
+<div id="asesoria" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7564.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Ramirez L. Yerlin E.</h4>
+    Jefa del departamento de asesoria legal
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+
+<!--################PRESUPUESTO#####################-->
+
+<div id="presupuesto" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7526.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Acosta Quintero Jorge B.</h4>
+    jefe de la unidad de planificacion y presupuesto
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################INFORMATICA#####################-->
+
+<div id="informatica" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/16316852.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Pierdahita Alzate Denison F.</h4>
+    Jefe del departamento de informatica
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################OBRERO#####################-->
+
+<div id="obrero" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7543.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Mendoza Zambrano Milagros</h4>
+    Jefa del departamento de obreros
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################EMPLEADO#####################-->
+
+<div id="empleado" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7586.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Gil Meza julissa del C.</h4>
+    Jefa del departamento de empleados
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################JUVILADO#####################-->
+
+<div id="jubilado" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/mariani.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Mariani</h4>
+    Jefa del departamento de jubilados
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+
+<!--################PASIVO#####################-->
+
+<div id="pasivo" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7636.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Bazan Davila Nelly C.</h4>
+    Jefa del departamento de pasivos laborales
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
+
+<!--################ADMINISTRACION#####################-->
+
+<div id="admin" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="media">
+  <div class="media-left media-middle">
+    <a href="#">
+      <img src="img/contacto/DSCN7517.jpg" alt="" class="media-object">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">D' Jesus Avila Marino</h4>
+    Jefe fel fepartamento de administracion
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+<!--#####################################-->
