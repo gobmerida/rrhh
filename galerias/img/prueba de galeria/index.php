ae53b90f37e29103f867ac0f41701fed7adb7b1e
Eduardo <eduardosaes@hotmail.com>
2016-07-25 08:25:41 -0430

    agregado nombre de galeria

--- galerias/img/prueba de galeria/index.php
+++ galerias/img/prueba de galeria/index.php
@@ -39,7 +39,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-              <li><a href="index.php">Inicio <span class="sr-only">(current)</span></a></li>
+              <li><a href="../../../">Inicio <span class="sr-only">(current)</span></a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quienes somos<span class="caret"></span></a>
                 <ul class="dropdown-menu">
@@ -138,7 +138,7 @@
         $("#main").load("../navegacion/galeria.php");
       });
       $(".contacto").click(function(){
-        $("#main").load("../contacto.php");
+        $("#main").load("../navegacion/contacto.php");
       });
     });
 
